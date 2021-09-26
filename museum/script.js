dc80fcacb03b36ed4a92a509dc94e16e9e27763a
Diana26 <plohayadamochka@gmail.com>
2021-09-26 20:57:36 +0300

    feat: slider welcome

---
+++ museum/script.js
@@ -0,0 +1,64 @@
+const pred = document.querySelector('.left');
+    next = document.querySelector('.right');
+    slides = document.querySelectorAll('.slide');
+    cubes = document.querySelectorAll('.cub');
+    numbs = document.querySelectorAll('.item');
+
+let index = 0;
+
+const activeSlide = n => {
+    for (slide of slides) {
+        slide.classList.remove('active');
+    }
+    slides[n].classList.add('active');
+}
+
+const activeNumb = n => {
+    for (numb of numbs) {
+        numb.classList.remove('active');
+    }
+    numbs[n].classList.add('active');
+}
+
+const activeCub = n => {
+    for (cub of cubes) {
+        cub.classList.remove('active');
+    }
+    cubes[n].classList.add('active');
+}
+
+const makeActive = ind => {
+    activeCub(ind);
+    activeSlide(ind);
+    activeNumb(ind);
+}
+
+const nextSlide = () => {
+    if (index == slides.length - 1) {
+        index = 0;
+        makeActive(index);
+    } else {
+        index++;
+        makeActive(index);
+    }
+}
+
+const predSlide = () => {
+    if (index == 0) {
+        index = slides.length - 1;
+        makeActive (index);
+    } else {
+        index--;
+        makeActive (index);
+    }
+}
+
+next.addEventListener('click', nextSlide);
+pred.addEventListener('click', predSlide);
+
+cubes.forEach((item, indexCub) => {
+    item.addEventListener('click', () => {
+    index = indexCub;
+    makeActive(index);
+    });
+})
