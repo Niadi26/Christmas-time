99b8c971f04bf3aa4381fbdd2ce472e41ab1e7da
Diana26 <plohayadamochka@gmail.com>
2021-10-11 22:16:54 +0300

    feat: explore, map, gallery

--- museum/script.js
+++ museum/script.js
@@ -158,11 +158,12 @@ swipeWelcome(el);
 const play = document.querySelector('#play');
 const playBtn = document.querySelector('.video_play_btn');
 const pause = document.querySelector('#pause');
-    sound = document.querySelector('#sound');
-    not_sound = document.querySelector('#not-sound');
-    full = document.querySelector('#full');
-    not_full = document.querySelector('#not-full');
-    let video = document.querySelector('#video_player');
+const sound = document.querySelector('#sound');
+const not_sound = document.querySelector('#not-sound');
+const full = document.querySelector('#full');
+const not_full = document.querySelector('#not-full');
+let video = document.querySelector('#video_player');
+let videoDiv = document.querySelector('.video_main');
 
 function playV() {
     video.play();
@@ -211,13 +212,13 @@ sound.addEventListener('click', muteV);
 not_sound.addEventListener('click', volumeV);
 
 function fullV () {
-    video.requestFullscreen();
+    videoDiv.requestFullscreen();
     full.classList.add('.hide');
     not_full.classList.remove('.hide');
 }
 
 function fullOutV () {
-    video.canselFullscreen();
+    videoDiv.canselFullscreen();
     full.classList.remove('.hide');
     not_full.classList.add('.hide');
 }
@@ -227,10 +228,10 @@ not_full.addEventListener('click', fullOutV);
 
 //работа с клавиатурой
 document.documentElement.addEventListener("keyup", function(e) {
+    event.preventDefault();
     let ev = e || window.event;
     console.log(ev.keyCode);
     if(ev.keyCode == 32) {
-        ev.preventDefault();
         if(video.paused) { playV(); }
         else { pauseV(); }
     } 
@@ -239,7 +240,7 @@ document.documentElement.addEventListener("keyup", function(e) {
         else {muteV()}
     } 
     else if(ev.keyCode == 70) {
-        if(video.fullscreenEnabled) {fullOutV();}                            //НЕ ВЫХОДИТ 
+        if(videoDiv.fullscreenEnabled) {fullOutV();}                            //НЕ ВЫХОДИТ 
         else {fullV()}
     } 
  //   else if(ev.keyCode == 188) {
@@ -277,5 +278,154 @@ function WindV () {
     playV();
 }
 
+//slider
 
-//console.log('Дневник с фронта. У нас следующие потерянные войны: 1. Плавная анимация в велком (-4балла), 2. Панель управления в полноэкранном режиме (-2балла),')
+function initComparisons() {
+    let x, i;
+    /* Найти все элементы с "overlay" класс: */
+    x = document.getElementsByClassName("img_overlay");
+    for (i = 0; i < x.length; i++) {
+      /* Один раз для каждого элемента "overlay":
+      передайте элемент "overlay" в качестве параметра при выполнении функции сравнения изображений: */
+      compareImages(x[i]);
+    }
+    function compareImages(img) {
+      /* Получить ширину и высоту элемента img */
+    let w = img.offsetWidth;
+    let h = img.offsetHeight;
+      /* Установите ширину элемента img равным 50%: */
+      img.style.width = (w / 2) + "px";
+      /* Создать слайдер: */
+      slider = document.createElement("DIV");
+      slider.setAttribute("class", "img_slider");
+      /* Вставить ползунок */
+      img.parentElement.insertBefore(slider, img);
+      /* Расположите ползунок по середине: */
+      slider.style.left = (w / 2) - (slider.offsetWidth / 2) + "px";
+      /* Выполнение функции при нажатии кнопки мыши: */
+      slider.addEventListener("mousedown", slideReady);
+      /* И еще одна функция, когда кнопка мыши отпущена: */
+      window.addEventListener("mouseup", slideFinish);
+      /* Или коснулся (для сенсорных экранов: */
+      slider.addEventListener("touchstart", slideReady);
+       /* И выпустили (для сенсорных экранов: */
+      window.addEventListener("touchstop", slideFinish);
+      function slideReady(e) {
+        /* Предотвратите любые другие действия, которые могут возникнуть при перемещении по изображению: */
+        e.preventDefault();
+        /* Теперь ползунок нажат и готов к перемещению: */
+        clicked = 1;
+        /* Выполнение функции при перемещении ползунка: */
+        window.addEventListener("mousemove", slideMove);
+        window.addEventListener("touchmove", slideMove);
+      }
+      function slideFinish() {
+        /* Ползунок больше не нажимается: */
+        clicked = 0;
+      }
+      function slideMove(e) {
+        let pos;
+        /* Если ползунок больше не нажимается, выйдите из этой функции: */
+        if (clicked == 0) return false;
+        /* Получить позицию курсора x: */
+        pos = getCursorPos(e)
+        /* Не допускайте позиционирования ползунка за пределами изображения: */
+        if (pos < 0) pos = 0;
+        if (pos > w) pos = w;
+        /* Выполните функцию, которая изменит размер наложенного изображения в соответствии с курсором: */
+        slide(pos);
+      }
+      function getCursorPos(e) {
+        let a, x = 0;
+        e = e || window.event;
+        /* Получить x позиции изображения: */
+        a = img.getBoundingClientRect();
+        /* Вычислить координату x курсора относительно изображения: */
+        x = e.pageX - a.left;
+        /* Рассмотрим любую прокрутку страницы: */
+        x = x - window.pageXOffset;
+        return x;
+      }
+      function slide(x) {
+        /* Измените размер изображения: */
+        img.style.width = x + "px";
+        /* Расположите ползунок: */
+        slider.style.left = img.offsetWidth - (slider.offsetWidth / 2) + "px";
+      }
+    }
+  }
+initComparisons();
+
+// MAP
+
+mapboxgl.accessToken = `pk.eyJ1IjoibmlhZGkyNiIsImEiOiJja3Vtc25hMHIwbW51Mm90aGs0cmU4dGI2In0.iQK4vFnkhikBr1_56sqbIg`;
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/niadi26/ckumuznxe1eww17m0axzf8ksm', // style URL
+    center: [2.3364, 48.86091], // starting position [lng, lat]
+    zoom: 16 // starting zoom
+    });
+
+    const marker1 = new mapboxgl.Marker({ color: 'black'})
+    .setLngLat([2.3364, 48.86091])
+    .addTo(map);
+     
+    const marker2 = new mapboxgl.Marker({ color: 'gray'})
+    .setLngLat([2.3333, 48.8602])
+    .addTo(map);
+    
+    const marker3 = new mapboxgl.Marker({ color: 'gray'})
+    .setLngLat([2.3397, 48.8607])
+    .addTo(map);   
+    
+    const marker4 = new mapboxgl.Marker({ color: 'gray'})
+    .setLngLat([2.3330, 48.8619])
+    .addTo(map);
+
+    const marker5 = new mapboxgl.Marker({ color: 'gray'})
+    .setLngLat([2.3365, 48.8625])
+    .addTo(map);
+
+    map.addControl(new mapboxgl.NavigationControl());
+
+// калькулятор
+function checkbox () {
+    let prise = document.querySelectorAll('input[name="ticketType"]:checked');
+    prise.onclick = function(e){
+    if (e.is(':checked')){
+        console.log(e.value);
+    }
+}
+}
+
+// Анимация выплывания
+
+function debounse (func, wait = 20, immediate = true) {
+    let timeout;
+    return function() {
+        let context = this, args = arguments;
+        let later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        }
+        let callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    }
+}
+
+const galleryImgs = document.querySelectorAll('.gallery_img');
+const gallery = document.querySelector('.gallery_content');
+function checkSlide (e) {
+    galleryImgs.forEach(sliderImg => {
+        const slideInAt = (window.scrollY + window.innerHeight);
+        const isHalfShow = slideInAt > gallery.offsetTop;
+        if (isHalfShow) {
+            sliderImg.classList.add('gallery_active');
+        } else {sliderImg.classList.remove('gallery_active');}
+    })
+}
+window.addEventListener('scroll', debounse(checkSlide));
+
+//console.log('Дневник с фронта. У нас следующие потерянные войны: 1. Плавная анимация в велком (-4балла), 2. Видео-слайдер, не хватило сил (-20баллов),')
